bb457a1554a8dc306caaee2cb7f6d17062f3bd06
rinooooo <rino.0566@gmail.com>
2022-07-26 15:05:27 +0900

    FactoryBotの書き方変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,8 +7,8 @@ FactoryBot.define do
     email                 { Faker::Internet.free_email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
-    first_name            { Faker::Name.first_name }
-    last_name             { Faker::Name.last_name }
+    first_name            { person.first }
+    last_name             { person.last }
     first_name_kana       { person.first.katakana }
     last_name_kana        { person.last.katakana }
     birth_date            { Faker::Date.between(from: '1930-01-01', to: 5.year.from_now) }
