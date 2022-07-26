83a03cf0621792a9a07c1b3549ae6a5888d34444
rinooooo <rino.0566@gmail.com>
2022-07-27 00:42:32 +0900

    Itemモデルの単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,23 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
 
-  validates :name, presence: true
+  validates :image, presence: true, blob: { content_type: :image }
+
+  validates :name, presence: true 
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+    
 
   with_options presence: true, format: { with: /\A[0-9]+\z/, message: '半角数値を使用してください' } do
     validates :item_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  belongs_to :user
-  has_one_attached :image
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
@@ -21,7 +25,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :deliveryCharge
   belongs_to_active_hash :deliveryArea
   belongs_to_active_hash :deliveryDay
-
 
 
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name                 { Faker::Name.name }
+    description          { Faker::Lorem.sentence }
+    category_id          { Faker::Number.between(from: 2, to: 11) }
+    condition_id         { Faker::Number.between(from: 2, to: 8) }
+    delivery_charge_id   { Faker::Number.between(from: 2, to: 48) }
+    delivery_area_id     { Faker::Number.between(from: 2, to: 3) }
+    delivery_day_id      { Faker::Number.between(from: 2, to: 4) }
+    item_price           { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
+
   end
 end
