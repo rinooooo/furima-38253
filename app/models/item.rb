db9038239af353b999617296ef4e4b4748a73fe2
rinooooo <rino.0566@gmail.com>
2022-07-26 16:56:50 +0900

    Itemモデル、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :delivery_area_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :item_price, presence: true
+
+
+
 end
