a63abb6b30124b76e751755f63f85e274c49df9b
rinooooo <rino.0566@gmail.com>
2022-07-26 18:06:51 +0900

    アクティブハッシュの設定(condition)

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,17 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :delivery_area_id, presence: true
-  validates :delivery_day_id, presence: true
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_price, presence: true
 
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
 
 
 
