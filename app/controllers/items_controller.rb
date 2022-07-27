643e85e6a53c0a372a4a567529f9aa26d7cbb644
rinooooo <rino.0566@gmail.com>
2022-07-27 21:50:18 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path current_user.id != @item.user.id
+    redirect_to root_path unless current_user.id == @item.user.id
   end
 
   def update
