44d5dcb547c9b662a228a303dacd078ecc828867
rinooooo <rino.0566@gmail.com>
2022-07-28 13:44:22 +0900

    商品購入ページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "items#index"
-  resources :items
-    resources :order, only: [:index]
-
+  resources :items do
+    resources :orders, only: [:index]
+  end
   
 end
