da56802d40ec68a49a5ae8adbe15b6f17458d2f9
rinooooo <rino.0566@gmail.com>
2022-07-28 13:46:15 +0900

    READMEの更新、カラムの変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @item = Item.find(params[:item.id])
     @delivery_card = DeliveryCard.new(order_params)
     if @delivery_card.valid?
       @delivery_card.save
@@ -13,7 +14,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:delivery_card).permit(:post_code, :delivery_area_id, :town, :address, :house_name, :phone_number, :card_number, :card_date, :card_security)
+    params.require(:delivery_card).permit(:post_code, :delivery_area_id, :town, :address, :house_name, :phone_number, :card_number, :card_date_month, :card_date_year, :card_security).merge(user_id: current_user.id, item_id: @item.id)
   end
 
 end
--- app/models/delivery_card.rb
+++ app/models/delivery_card.rb
@@ -1,6 +1,6 @@
 class DeliveryCard
   include ActiveModel::Model
-    attr_accessor :post_code, :delivery_area_id, :town, :address, :house_name, :phone_number, :card_number, :card_date, :card_security, :order_id
+    attr_accessor :post_code, :delivery_area_id, :town, :address, :house_name, :phone_number, :card_number, :card_date_month, :card_date_year, :card_security, :order_id
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,7 +8,8 @@ class DeliveryCard
     validates :address
     validates :phone_number
     validates :card_number
-    validates :card_date
+    validates :card_date_month
+    validates :caed_date_year
     validates :card_security
   end
   validates :delivery_area_id, numericality: {other_than: 0, message: "can't be blank"}
