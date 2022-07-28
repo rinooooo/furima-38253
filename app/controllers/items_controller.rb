21be5f2a01c945123d840d9a4f20fa6c18518f77
rinooooo <rino.0566@gmail.com>
2022-07-28 23:11:05 +0900

    削除機能の実装前に戻す

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :find_params, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :find_params, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -32,11 +32,6 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
-  end
-
-  def destroy
-    @item.destroy
-    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "items#index"
-  resources :items do
+  resources :items, only: [:index, :new, :create, :show, :edit, :update] do
     resources :orders, only: [:index, :create]
   end
   
