d93e5cb462f8d7dea397d18f986cac7644d1e44f
rinooooo <rino.0566@gmail.com>
2022-07-28 17:45:23 +0900

    他機能関連

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,15 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_params, only: [:index, :create]
 
   def index
-    redirect_to root_path unless current_user.id != @item.user.id
-    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user.id || @item.order.present?
+      redirect_to root_path
+    end
     @delivery_card = DeliveryCard.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @delivery_card = DeliveryCard.new(order_params)
     if @delivery_card.valid?
       @delivery_card.save
@@ -23,6 +24,11 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:delivery_card).permit(:post_code, :delivery_area_id, :town, :address, :house_name, :phone_number, :card_number, :card_date_month, :card_date_year, :card_security).merge(user_id: current_user.id, item_id: @item.id)
   end
+
+  def find_params
+    @item = Item.find(params[:item_id])
+  end
+
 
 
 end
