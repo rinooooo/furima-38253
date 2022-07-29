b03ffa8ed4906020770943347d1c5326eb686e38
rinooooo <rino.0566@gmail.com>
2022-07-29 15:28:30 +0900

    rubocopの実行

--- spec/models/delivery_card_spec.rb
+++ spec/models/delivery_card_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe DeliveryCard, type: :model do
   before do
     user = FactoryBot.create(:user)
-    item= FactoryBot.create(:item)
+    item = FactoryBot.create(:item)
     @delivery_card = FactoryBot.build(:delivery_card, user_id: user.id, item_id: item.id)
   end
 
