1fa956dccfdbcde780b7e823eb5580d20dd0b7bc
rinooooo <rino.0566@gmail.com>
2022-08-01 10:29:34 +0900

    商品出品時のエラーハンドリング修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,6 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :item_price, presence: true
   with_options presence: true do
     validates :item_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
