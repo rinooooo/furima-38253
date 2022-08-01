aeaa8f0f8198a909ebc18c71cd3bbfc15cc5ddc4
rinooooo <rino.0566@gmail.com>
2022-08-01 10:29:46 +0900

    conditionアクティブハッシュ修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name                 { Faker::Name.name }
     description          { Faker::Lorem.sentence }
     category_id          { Faker::Number.between(from: 2, to: 11) }
-    condition_id         { Faker::Number.between(from: 2, to: 8) }
+    condition_id         { Faker::Number.between(from: 2, to: 7) }
     delivery_charge_id   { Faker::Number.between(from: 2, to: 3) }
     delivery_area_id     { Faker::Number.between(from: 2, to: 48) }
     delivery_day_id      { Faker::Number.between(from: 2, to: 4) }
